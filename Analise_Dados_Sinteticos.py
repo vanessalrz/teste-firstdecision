96f87536cee08e5e3cd3eebe16fd7ee166d91ab6
vanessalrz <vanessalorenzonii@gmail.com>
2024-06-22 23:42:57 -0300

    Adicionado alguns comentários e limpeza de código.

--- Analise_Dados_Sinteticos.py
+++ Analise_Dados_Sinteticos.py
@@ -4,6 +4,7 @@ import pandas as pd
 import seaborn as sns
 from babel.numbers import format_currency
 
+#
 def grafico_barras(df, x, y, xlabel, ylabel, title, color, formato_moeda=False, figsize=(12, 6)):
     plt.figure(figsize=figsize)
     ax = sns.barplot(data=df, x=x, y=y, palette=color, orient='h')
@@ -34,7 +35,7 @@ def grafico_pizza(df, x, y, title, color, formato_moeda=False, figsize=(10, 6)):
 
 plt.rc('font', family='serif')
 
-diretorio = 'dados_vendas_sinteticos'
+diretorio = 'dados_vendas_sinteticos' # Diretório para armazenar os arquivos CSV
 dfs = []
 
 for arquivo in os.listdir(diretorio):
@@ -42,21 +43,24 @@ for arquivo in os.listdir(diretorio):
         caminho_arquivo = os.path.join(diretorio, arquivo)
         try:
             dfs.append(pd.read_csv(caminho_arquivo))
-        except pd.errors.EmptyDataError:
+        except pd.errors.EmptyDataError: # verificando se o arquivo é vazio e se for, ignora o arquivo
             print(f"O arquivo {arquivo} está vazio e será ignorado.")
-        except Exception as e:
+        except Exception as e: # caso de erro, imprime o erro
             print(f"Erro ao ler o arquivo {arquivo}: {e}")
 
+# Verificando se os arquivos foram carregados corretamente
 if not dfs:
     raise ValueError("Nenhum dado CSV foi carregado.")
 
+# Concatenando os dataframes
 df_completo = pd.concat(dfs, ignore_index=True)
 
+# Verificando se o dataframe contém as colunas 'Região', 'Vendas' e 'Produto'
 if 'Região' not in df_completo.columns or 'Vendas' not in df_completo.columns or 'Produto' not in df_completo.columns:
     raise ValueError("O dataframe não contém as colunas 'Região', 'Vendas' ou 'Produto'.")
 
 # Total de vendas por região
-total_vendas_por_regiao = df_completo.groupby('Região')['Vendas'].sum().reset_index()
+total_vendas_por_regiao = df_completo.groupby('Região')['Vendas'].sum().reset_index() # Agrupando por região e somando as vendas, criando um novo dataframe e resetando o index
 
 # Identificação dos produtos mais vendidos
 produtos_mais_vendidos = df_completo['Produto'].value_counts().reset_index()
@@ -64,17 +68,15 @@ produtos_mais_vendidos.columns = ['Produto', 'Quantidade']
 produtos_mais_vendidos = produtos_mais_vendidos.sort_values(by='Quantidade', ascending=False)
 produtos_mais_vendidos_valor = df_completo.groupby('Produto')['Vendas'].sum().reset_index().sort_values(by='Vendas', ascending=False)
 
-
+# Criando os graficos de barras
 grafico_barras(total_vendas_por_regiao, 'Vendas', 'Região', 'Vendas (R$)', 'Região', 'Total de Vendas por Região', plt.cm.Set3.colors, formato_moeda=True)
 grafico_barras(produtos_mais_vendidos, 'Quantidade', 'Produto', 'Quantidade', 'Produtos', 'Produtos mais Vendidos - Quantidade', plt.cm.Set2.colors, formato_moeda=False)
 grafico_barras(produtos_mais_vendidos_valor, 'Vendas', 'Produto', 'Vendas (R$)', 'Produtos', 'Produtos mais Vendidos - Valor', plt.cm.Set2.colors, formato_moeda=True)
 
-total_vendas = total_vendas_por_regiao['Vendas'].sum()
+# Criando os graficos de pizza
 grafico_pizza(total_vendas_por_regiao, 'Região', 'Vendas', 'Distribuição do Total de Vendas por Região', plt.cm.Set3.colors, formato_moeda=True)
 grafico_pizza(produtos_mais_vendidos, 'Produto', 'Quantidade', 'Distribuição dos Produtos mais Vendidos', plt.cm.Set2.colors, formato_moeda=False)
 
-total_vendas_por_regiao['Vendas'] = total_vendas_por_regiao['Vendas'].round(2)
-produtos_mais_vendidos['Quantidade'] = produtos_mais_vendidos['Quantidade'].round(2)
-
-total_vendas_por_regiao.to_csv('total_vendas_por_regiao.csv', index=False)
-produtos_mais_vendidos.to_csv('produtos_mais_vendidos.csv', index=False)
+# Exportar os dataframes para arquivos CSV
+total_vendas_por_regiao.round(2).to_csv('total_vendas_por_regiao.csv', index=False)
+produtos_mais_vendidos.round(2).to_csv('produtos_mais_vendidos.csv', index=False)
