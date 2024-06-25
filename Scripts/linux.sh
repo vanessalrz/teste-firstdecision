511fc322ef58761fb17fcba64bb90ad55ce51959
vanessalrz <vanessalorenzonii@gmail.com>
2024-06-25 16:06:11 -0300

    adc conteudo ao linux.sh

--- Scripts/linux.sh
+++ Scripts/linux.sh
@@ -15,11 +15,39 @@ check_services() {
   done
 }
 
+# Chama a função para verificar os servicos
 check_services
 
 # Função para realizar backups do diretório dados_vendas_sinteticos e salvar no diretório backup.
 backup_files() {
   tar -czf backup/dados_vendas_sinteticos_$(date +%F).tar.gz ../dados_vendas_sinteticos
+  # enviar para object storage para armazenamento de longa data (cold storage), como S3 da amazon.
+  # remover o .tar.gz após backup bem sucedido
 }
 
+# Chama a função para realizar backups
 backup_files
+
+# Função para monitorar o uso de recursos (CPU, memória, espaço em disco).
+# Esta função imprime as informações de CPU, memória, espacamento em disco e uso de memória.
+monitor_resources() {
+  echo "Uso de CPU e memória:"
+  top -b -n1 | head -n 10
+
+  echo "Espaço em disco:"
+  df -h
+
+  echo "Uso de memória:"
+  free -h
+}
+
+# Chama a função para monitorar os recursos
+monitor_resources
+
+# Função para verificar e instalar atualizações de segurança
+# Pode usar sem o sudo caso voce seja root, o sudo permite executar com privilegios de superusuario,
+update_system() {
+  sudo apt update && apt list --upgradable
+  sudo apt upgrade -y
+}
+
