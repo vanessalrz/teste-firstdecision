8553e627578d0db492746549ec2acf01982a6d86
vanessalrz <vanessalorenzonii@gmail.com>
2024-06-26 22:14:27 -0300

    final

--- exercicio-2/app/models.py
+++
@@ -1,9 +0,0 @@
-from app import db
-
-class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    task = db.Column(db.String(120), nullable=False)
-
-    def __repr__(self):
-        return f'<Todo {self.task}>'
-    
